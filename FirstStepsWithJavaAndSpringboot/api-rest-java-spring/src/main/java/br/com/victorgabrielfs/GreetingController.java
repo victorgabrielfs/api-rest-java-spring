8860c204462c7db96f500ec7018a11137d360f91
Victor <victor.g.filgueiras@gmail.com>
2024-08-14 11:24:22 -0300

    Primeiro Hello World

---
+++ FirstStepsWithJavaAndSpringboot/api-rest-java-spring/src/main/java/br/com/victorgabrielfs/GreetingController.java
@@ -0,0 +1,19 @@
+package br.com.victorgabrielfs;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private final String template = "Hello, %s!";
+    private AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
